32fb5b4316f232a243f16f57b2474ecdcc3e6510
M1ndst0rM <12268955+yM1ndst0rM@users.noreply.github.com>
2024-08-18 22:20:39 +0200

    Implement ability to place sigils

--- Scripts/AbilityPlaceSigil.cs
+++ Scripts/AbilityPlaceSigil.cs
@@ -1,15 +1,41 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using GMTKGJ2024.resources;
 
-public partial class AbilityPlaceSigil : Node
+public partial class AbilityPlaceSigil : Marker2D
 {
-	
-	public override void _Ready()
+	[Export] public SigilConf SigilConfig { get; set; }
+
+	private List<Node2D> _placedSigils = new();
+	private List<int> _usnusedInidices = new();
+	private Random _rnd = new();
+
+	private void PlaceRandomSigil()
 	{
+		var randomIndex = _rnd.Next(_usnusedInidices.Count - 1);
+		var nextIndex = _usnusedInidices[randomIndex];
+		_usnusedInidices.RemoveAt(randomIndex);
+		
+		var sigilBlueprint = SigilConfig.SigilInventory[nextIndex];
+		var sigil = sigilBlueprint.Instantiate<Node2D>();
+		Owner.AddChild(sigil);
+		sigil.GlobalTransform = GlobalTransform;
+		sigil.Rotation = _rnd.Next(400);
+		_placedSigils.Add(sigil);
+
+		if (_placedSigils.Count >= SigilConfig.SigilCountForFullSpell)
+		{
+			//Cast spell
+			Reset();
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void Reset()
 	{
+		_usnusedInidices.Clear();
+		_usnusedInidices.AddRange(Enumerable.Range(0, SigilConfig.SigilInventory.Count - 1));
+		_placedSigils.Clear();
 	}
 }
