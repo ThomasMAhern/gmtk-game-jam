846428c7cd599e0ae5a35c6fc638e460356a7642
M1ndst0rM <12268955+yM1ndst0rM@users.noreply.github.com>
2024-08-18 18:18:46 +0200

    Recreate player movement in cs script for ease of use

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,7 +1,30 @@
+using GMTKGJ2024.resources;
 using Godot;
 
 namespace GMTKGJ2024.Scripts;
 
 public partial class Player : CharacterBody2D
 {
+    [Export] public PlayerStats Stats { get; set; }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (Input.IsActionPressed("ui_right"))
+        {
+              Velocity = Vector2.Right * Stats.Speed;
+        } else if (Input.IsActionPressed("ui_down"))
+        {
+              Velocity = Vector2.Down * Stats.Speed;
+        } else if (Input.IsActionPressed("ui_left"))
+        {
+              Velocity = Vector2.Left * Stats.Speed;
+        } else if (Input.IsActionPressed("ui_up"))
+        {
+              Velocity = Vector2.Up * Stats.Speed;
+        } else {
+              Velocity = Vector2.Zero;
+        }
+        
+        MoveAndSlide();
+    }
 }
