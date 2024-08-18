7bf15110a73e8bee2a309ba7b37d0d304c445ad9
M1ndst0rM <12268955+yM1ndst0rM@users.noreply.github.com>
2024-08-18 15:17:24 +0200

    Create script to emit scale events according to the burst scale config

---
+++ Scripts/BurstScaleEmitterEffect.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace GMTKGJ2024.Scripts;
+
+public partial class BurstScaleEmitterEffect : Node
+{
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
